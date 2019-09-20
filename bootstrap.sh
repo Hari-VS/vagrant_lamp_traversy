c230fb7fb21196babb1299ff2574b9814e777a91
Hari-VS <harivs93@gmail.com>
2019-09-20 20:01:01 +0530

    Update OS and PHP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,44 +1,46 @@
-# Update Packages
+#Update Packages
 apt-get update
-# Upgrade Packages
-apt-get upgrade
 
-# Basic Linux Stuff
+#Upgrade Packages
+apt-get upgrade
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive
+
+#Install Git
 apt-get install -y git
 
-# Apache
+#Install Apache
 apt-get install -y apache2
 
-# Enable Apache Mods
+#Enable Apache Mods
 a2enmod rewrite
 
-#Add Onrej PPA Repo
+#Add Ondrej PPA Repo
 apt-add-repository ppa:ondrej/php
 apt-get update
 
-# Install PHP
-apt-get install -y php7.2
+#Install PHP
+apt-get install -y php7.3
 
-# PHP Apache Mod
-apt-get install -y libapache2-mod-php7.2
+#PHP Apache Mod
+apt-get install -y libapache2-mod-php7.3
 
-# Restart Apache
-service apache2 restart
+#Restart Apache
+sudo service apache2 restart
 
-# PHP Mods
-apt-get install -y php7.2-common
-apt-get install -y php7.2-mcrypt
-apt-get install -y php7.2-zip
+#Install PHP Mods
+apt-get install -y php7.3-common
+apt-get install -y php7.3-mcrypt
+apt-get install -y php7.3-zip
 
-# Set MySQL Pass
+#Set MYSQL Pass for user root
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-# Install MySQL
+#Install MYSQL
 apt-get install -y mysql-server
 
-# PHP-MYSQL lib
-apt-get install -y php7.2-mysql
+#PHP-MYSQL lib
+apt-get install -y php7.3-mysql
 
-# Restart Apache
+#Restart Apache
 sudo service apache2 restart
